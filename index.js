6097da17411ee10130d1b273db7bcf303492d902
Siddhartha Vemuri <svemuri8@gatech.edu>
2018-12-03 17:09:47 -0500

    Fix formatting

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var config = {
-	apiKey: "AIzaSyCJYrOi7UhuSxF4LZFjvrpNWVklNB7Lp1k",
-	authDomain: "empower101010.firebaseapp.com",
-	databaseURL: "https://empower101010.firebaseio.com",
-	projectId: "empower101010",
-	storageBucket: "empower101010.appspot.com",
-	messagingSenderId: "341616600829",
+    apiKey: "AIzaSyCJYrOi7UhuSxF4LZFjvrpNWVklNB7Lp1k",
+    authDomain: "empower101010.firebaseapp.com",
+    databaseURL: "https://empower101010.firebaseio.com",
+    projectId: "empower101010",
+    storageBucket: "empower101010.appspot.com",
+    messagingSenderId: "341616600829",
 };
 
 firebase.initializeApp(config);
@@ -14,76 +14,76 @@ var database = firebase.database();
 
 var list = [];
 function start(){
-	database.ref().once('value', function(snapshot) {
-		snapshot.forEach(function(childSnapshot) {
-			snap = childSnapshot.val();
-			list.push(snap);
+    database.ref().once('value', function(snapshot) {
+	snapshot.forEach(function(childSnapshot) {
+	    snap = childSnapshot.val();
+	    list.push(snap);
 
-	    });
 	});
+    });
 }
 start();
 function createNewUser(first, last, age, device, zip){
-	  firebase.database().ref(list.length + 1).set({
-	    Age: age,
-	    First_Name: first,
-	    Last_Name: last,
-	    Device: device,
-	    Zipcode: zip
-  });
+    firebase.database().ref(list.length + 1).set({
+	Age: age,
+	First_Name: first,
+	Last_Name: last,
+	Device: device,
+	Zipcode: zip
+    });
 }
 
 
 function show(){
-	list.sort(function(a, b){
-	  return a.Age - b.Age;
-	});
-	for (var i = 0; i <= list.length-1; i++) {
-		first = list[i].First_Name;
-		last = list[i].Last_Name;
-		age = list[i].Age;
-	        zip = list[i].Zipcode;
-                device = list[i].Device
+    list.sort(function(a, b){
+	return a.Age - b.Age;
+    });
+    for (var i = 0; i <= list.length-1; i++) {
+	first = list[i].First_Name;
+	last = list[i].Last_Name;
+	age = list[i].Age;
+	zip = list[i].Zipcode;
+        device = list[i].Device
 
-		z = document.createElement('tr');
+	z = document.createElement('tr');
 
-   		a = document.createElement('td');
-   		b = document.createElement('td');
-   		c = document.createElement('td');
-   	        d = document.createElement('td');
-                e = document.createElement('td');
+   	a = document.createElement('td');
+   	b = document.createElement('td');
+   	c = document.createElement('td');
+   	d = document.createElement('td');
+        e = document.createElement('td');
 
-   		a.innerHTML = first;
-   		b.innerHTML = last;
-   		c.innerHTML = age;
-   		d.innerHTML = zip;
-                e.innerHTML = device;
+   	a.innerHTML = first;
+   	b.innerHTML = last;
+   	c.innerHTML = age;
+   	d.innerHTML = zip;
+        e.innerHTML = device;
 
-   		z.appendChild(a);
-   		z.appendChild(b);
-   		z.appendChild(c);
-   	        z.appendChild(d);
-                z.appendChild(e);
+   	z.appendChild(a);
+   	z.appendChild(b);
+   	z.appendChild(c);
+   	z.appendChild(d);
+        z.appendChild(e);
 
-   		document.getElementById('here').appendChild(z);
-	}
-	document.getElementById('show').remove();
+   	document.getElementById('here').appendChild(z);
+    }
+    document.getElementById('show').remove();
 
 }
 
 function submit(){
 
-	form = document.getElementById('form');
-	first = form.elements[0].value;
-	last = form.elements[1].value;
-	age = form.elements[2].value;
-	device = form.elements[4].value;
-	zip = form.elements[3].value;
+    form = document.getElementById('form');
+    first = form.elements[0].value;
+    last = form.elements[1].value;
+    age = form.elements[2].value;
+    device = form.elements[4].value;
+    zip = form.elements[3].value;
 
 
-        createNewUser(first, last, age, device, zip, device);
-	document.getElementById('form').remove();
+    createNewUser(first, last, age, device, zip, device);
+    document.getElementById('form').remove();
 
-	location.reload();
+    location.reload();
 
 }
