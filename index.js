355fece4a79a47d502d4d65ebd74f113575d1d50
trunci <eduardotrunci@gmail.com>
2018-12-04 11:32:04 -0500

    Final changes

--- index.js
+++ index.js
@@ -182,3 +182,60 @@ function sortTable(n) {
     }
   }
 }
+
+
+function findPeople(){
+	form = document.getElementById('form1');
+    a = form.elements[0].value;
+    document.getElementById('here').innerHTML = ''
+
+
+    for (var i = list.length - 1; i >= 0; i--) {
+    	if (a == list[i].Zipcode){
+	    	first = list[i].First_Name;
+			last = list[i].Last_Name;
+			age = list[i].Age;
+			zip = list[i].Zipcode;
+	        device = list[i].Device;
+	        contact = list[i].Contact;
+	        severity = list[i].Condition_Severity;
+	        priority = list[i].Device_Priority;
+	        survivability = list[i].Survivability;
+
+			z = document.createElement('tr');
+
+		   	a = document.createElement('td');
+		   	b = document.createElement('td');
+		   	c = document.createElement('td');
+		   	d = document.createElement('td');
+		    e = document.createElement('td');
+	        f = document.createElement('td');
+	        g = document.createElement('td');
+	        h = document.createElement('td');
+	        j = document.createElement('td');
+
+		   	a.innerHTML = first;
+		   	b.innerHTML = last;
+		   	c.innerHTML = age;
+		   	d.innerHTML = zip;
+	        e.innerHTML = device;
+	        f.innerHTML = contact;
+	        g.innerHTML = severity;
+	        h.innerHTML = priority;
+	        j.innerHTML = survivability;
+
+		   	z.appendChild(a);
+		   	z.appendChild(b);
+		   	z.appendChild(c);
+		   	z.appendChild(d);
+	        z.appendChild(e);
+	        z.appendChild(f);
+	        z.appendChild(g);
+	        z.appendChild(h);
+	        z.appendChild(j);
+
+		   	document.getElementById('here').appendChild(z);
+	    }
+	}
+}
+
